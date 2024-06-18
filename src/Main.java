ba468768f934b26087a7b9f85585cb4bbe42eeb6
aabdurazakov005 <aabdurazakov005@yandex.ru>
2024-06-18 12:06:38 +0300

    Home work number two.

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        var cat = 3.6;
+        var dog = 8.0;
+        var paper = 763789;
+        System.out.println(cat);
+        System.out.println(dog);
+        System.out.println(paper);
+        cat = cat + 4;
+        dog = dog + 4;
+        paper = paper + 4;
+        System.out.println(cat);
+        System.out.println(dog);
+        System.out.println(paper);
+        cat = cat - 1.6;
+        dog = dog - 3.5;
+        paper = paper - 7639;
+        System.out.println(cat);
+        System.out.println(dog);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var totalWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println(totalWeight);
+        var weightDifference = firstBoxerWeight - secondBoxerWeight;
+        System.out.println(weightDifference);
+
+    }
+}
